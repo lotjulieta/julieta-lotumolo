e424ecb527cc14e83ee11f458abacb328ad4f4a4
Julieta <lot.julieta@gmail.com>
2024-02-20 21:37:49 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        dark: "#1a202c",
+        futuristic: "#00ffaa",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        roboto: ["Roboto condensed"],
+      }
+    },
+  },
+  plugins: ["@tailwindcss/typography"],
+};
