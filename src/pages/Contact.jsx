4eed91bd60b0e8b8e2a7b266efee4372e93a8330
Julieta <lot.julieta@gmail.com>
2024-02-22 12:25:28 -0300

    add form contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 
-export default function Contact(){
-    const [formData, setFormData] = useState({
+export default function Contact() {
+  const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: ''
@@ -26,65 +26,67 @@ export default function Contact(){
       message: ''
     });
   };
-    
-  return (
-         <div className="container mx-auto my-22">
-            <h1 className="text-4xl text-center mb-20 mt-20">Contact me!</h1>
-            <div className="flex flex-wrap">
-                <div className="lg:w-1/2 lg:pr-10 mb-4 lg:mb-0">
-      <img src="src\images\cute-robot-waving-hand-cartoon-icon-illustration-science-technology-icon-concept-isolated-premium-flat-cartoon-style-vector-Photoroom.png-Photoroom.png" alt="Your Image" className="mt-12 w-full lg:w-full mx-auto lg:mx-0 animate-bounce" />
-    </div>
-      <a
-        href="https://www.linkedin.com/in/julieta-lotumolo/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mr-2 mt-20"
-      >
-        <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          <img src="src\images\icons8-linkedin-240.png" alt="LinkedIn" className="h-6 w-6 mr-2 inline-block" />
-          LinkedIn
-        </button>
-      </a>
-      <a
-        href="https://github.com/lotjulieta"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mr-2 mt-20"
-      >
-        <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          <img src="src\images\icons8-github-96.png" alt="GitHub" className="h-6 w-6 mr-2 inline-block" />
-          GitHub
-        </button>
-      </a>
-    </div>
-    <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">
-              Nombre
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="name" type="text" placeholder="Tu Nombre" name="name" value={formData.name} onChange={handleChange} required />
-          </div>
-          <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
-              Correo Electrónico
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@example.com" name="email" value={formData.email} onChange={handleChange} required />
-          </div>
-          <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="message">
-              Mensaje
-            </label>
-            <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" placeholder="Escribe aquí tu mensaje..." name="message" value={formData.message} onChange={handleChange} required />
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Enviar Mensaje
-          </button>
-        </div>
-      </form>
-    </div>
 
-    )
+  return (
+   <div className="container mx-auto my-22 flex items-start">
+      <div className="w-full md:w-1/2">
+        <img src="src\images\cute-robot-waving-hand-cartoon-icon-illustration-science-technology-icon-concept-isolated-premium-flat-cartoon-style-vector-Photoroom.png-Photoroom.png" alt="Contact" className="h-auto w-full animate-bounce" style={{ marginTop: '230px', marginLeft: '-100px' }} />
+      </div>
+      <div className="w-full md:w-1/2">
+        <h1 className="text-4xl text-center mb-10 mt-10" style={{ marginLeft: '-460px' }}>Contact me!</h1>
+        <div className="flex flex-wrap justify-center">
+          <a
+            href="https://www.linkedin.com/in/julieta-lotumolo/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-2 mt-20"
+          >
+            <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              <img src="src\images\icons8-linkedin-240.png" alt="LinkedIn" className="h-6 w-6 mr-2 inline-block" />
+              LinkedIn
+            </button>
+          </a>
+          <a
+            href="https://github.com/lotjulieta"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-2 mt-20"
+          >
+            <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              <img src="src\images\icons8-github-96.png" alt="GitHub" className="h-6 w-6 mr-2 inline-block" />
+              GitHub
+            </button>
+          </a>
+        </div>
+        <p className='text-xl text-center mt-8 mb-8'>or send me an email</p>
+        <form onSubmit={handleSubmit} className="w-full max-w-md mt-4 mx-auto">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">
+                Nombre
+              </label>
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="name" type="text" placeholder="Tu Nombre" name="name" value={formData.name} onChange={handleChange} required />
+            </div>
+            <div className="w-full px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
+                Correo Electrónico
+              </label>
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@example.com" name="email" value={formData.email} onChange={handleChange} required />
+            </div>
+            <div className="w-full px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-2" htmlFor="message">
+                Mensaje
+              </label>
+              <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" placeholder="Escribe aquí tu mensaje..." name="message" value={formData.message} onChange={handleChange} required />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              Enviar Mensaje
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
