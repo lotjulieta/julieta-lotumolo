282d96563236b653ab9c2d47b83513926734f0dd
Julieta <lot.julieta@gmail.com>
2024-02-27 16:37:20 -0300

    latest changes

--- src/components/shared/Portada.jsx
+++ src/components/shared/Portada.jsx
@@ -13,11 +13,11 @@ export default function Portada() {
     <section className="bg-zinc-950 text-white py-10 lg:py-10 md:mt-12 md:mr-12 sm:mt-14">
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center">
         <div className="lg:w-2/3">
-          <h1 className="animate__animated animate__bounceInDown sm:text-5xl md:text-4xl lg:text-6xl text-center lg:text-left">Hello everyone!</h1> 
-          <p style={{ color: '#4b5563'}} className="animate__animated animate__bounceInLeft text-xl sm:text-3xl lg:text-4xl leading-relaxed text-center lg:text-left">
+          <h1 className="animate__animated animate__bounceInDown sm:text-8xl md:text-4xl lg:text-6xl text-center lg:text-left">Hello everyone!</h1> 
+          <p style={{ color: '#4b5563'}} className="animate__animated animate__bounceInLeft text-xl sm:text-6xl lg:text-4xl leading-relaxed text-center lg:text-left">
             My name is Julieta.
           </p>
-          <p className="animate__animated animate__bounceInLeft text-xl sm:text-2xl lg:text-3xl leading-relaxed text-center lg:text-left" style={{ color: '#6C63FF'}}>I´m a Backend Developer.</p>
+          <p className="animate__animated animate__bounceInLeft text-xl sm:text-6xl lg:text-4xl leading-relaxed text-center lg:text-left" style={{ color: '#6C63FF'}}>I´m a Backend Developer.</p>
           <div className="flex justify-center lg:justify-start">
             <button
               className="bg-gray-900 hover:bg-zinc-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 mb-16"
